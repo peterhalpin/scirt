11c29ca8be5d0807c5d8ffec631f811dc371d85d
peterhalpin <peter.halpin@nyu.edu>
2016-10-14 19:02:45 -0400

    more standard errors

--- R/WAI_tests.R
+++ R/WAI_tests.R
@@ -21,7 +21,6 @@ col_form[odd,] <- col_form[odd+1,] <- col_form[odd,]*col_form[odd+1,]
 ind_theta <- factor.scores(calib_ltm, ind_form, type = "EB", prior = F)$score.dat$z1
 col_theta <- factor.scores(calib_ltm, col_form, type = "EB", prior = F)$score.dat$z1
 barbell_plot(ind_theta, col_theta, legend = "left")
-barbell_plot
 
 parms <- coef(calib_ltm)
 beta_C <- parms[grep("C", row.names(parms)), 1]
@@ -48,27 +47,52 @@ WAI <- function(w, alpha, beta, theta1, theta2){
   w[1] * twoPL(alpha, beta, theta1) + w[2] * twoPL(alpha, beta, theta2) + (1 - w[1] - w[2]) * twoPL(alpha, beta, theta1) * twoPL(alpha, beta, theta2)
 }
 
-neglogGAI <- function(a, resp, alpha, beta, theta1, theta2){
-  w <- exp(a)/(1+exp(a))
+neglogGAI <- function(w, resp, alpha, beta, theta1, theta2){
+  #w <- exp(a)/(1+exp(a))
   p <- GAI(w, alpha, beta, theta1, theta2)
   p[p>1] <- .999
   - apply(log(p) * (resp) + log(1-p) * (1-(resp)), 1, sum, na.rm = T)
 }
 
 # sum to 1
-lambda1 <- function(a, resp, alpha, beta, theta1, theta2){
-  sum(exp(a)/(1+exp(a)))
+lambda1 <- function(w, resp, alpha, beta, theta1, theta2){
+  #sum(exp(a)/(1+exp(a)))
+  sum(w)
 }
 
 # between 1 and zero
-lambda2 <- function(a, resp, alpha, beta, theta1, theta2){
-  exp(a)/(1+exp(a))
+lambda2 <- function(w, resp, alpha, beta, theta1, theta2){
+  #exp(a)/(1+exp(a))
+  w
 }
-theta1 <- 0
-theta2 <- 1
+
+# ML for GAI ------------------------------------------------------------------
+
+x0 <- c(.25,.25,.25,.25)
+i = 1
+
+q <- solnp(x0, neglogGAI,
+   eqfun = lambda1,
+   eqB = 1,
+   ineqfun = lambda2,
+   ineqUB = rep(.9999, 4),
+   ineqLB = rep(.0001, 4),
+   resp = resp[i,],
+   alpha = alpha,
+   beta = beta,
+   theta1 = theta1[i],
+   theta2 = theta2[i])
+q
+P <- round(q$par, 4)
+sqrt(P*(1-P))
+
+ind <- which(P > .0001 & P < .9999)
+SE <- (solve(q$hessian[ind+4, ind+4]))
+P; sqrt(diag(SE))
+
+# Plotting SE ------------------------------------------------------------------
 
 SE <- function(w1, w2, alpha, beta, theta1, theta2){
-
   R <- WAI(c(w1, w2), alpha, beta, theta1, theta2)
   m <-  1 / sqrt(R * (1-R))
   P1 <- twoPL(alpha, beta, theta1)
@@ -94,30 +118,6 @@ plotSE2 <- function(theta1, theta2, alpha, beta, w1, w2){
    }
    mapply(temp, theta1, theta2)
 }
-# ML for GAI ------------------------------------------------------------------
-
-x0 <- c(0,0,0,0)
-i = 7
-
-q <- solnp(x0, neglogGAI,
-   eqfun = lambda1,
-   eqB = 1,
-   ineqfun = lambda2,
-   ineqUB = rep(.9999, 4),
-   ineqLB = rep(.0001, 4),
-   resp = resp[i,],
-   alpha = alpha,
-   beta = beta,
-   theta1 = theta1[i],
-   theta2 = theta2[i])
-q
-
-temp <- round(exp(q$pars)/(1+exp(q$pars)),3)
-ind <- which(temp > 0 & temp < 1) + 4
-temp
-sqrt(solve(q$hessian[ind, ind]))
-
-# Plotting SE ------------------------------------------------------------------
 
 w1 <- w2 <- seq(0, 1, by = .02)
 l <- outer(w1, w2, plotSE, alpha, beta, -1, 1)
@@ -127,6 +127,7 @@ theta1 <- theta2 <- seq(-2, 2, by = .1)
 theta1
 m <- outer(theta1, theta2, plotSE2, alpha, beta, 1, 1)
 m[m>10] <- NA
+
 
 persp(theta1, theta2, m, theta = -45, phi = 20, main = "SE", expand = .5, ticktype = "detailed", nticks = 5)
 
@@ -149,8 +150,8 @@ gradGAI <- function(w, resp, alpha, beta, theta1, theta2){
 
 # Plots for WAI
 
-WAI <- function(a, alpha, beta, theta1, theta2){
-  w <- exp(a)/(1+exp(a))
+WAI <- function(w, alpha, beta, theta1, theta2){
+  #w <- exp(a)/(1+exp(a))
   w[1] * twoPL(alpha, beta, theta1) + w[2] * twoPL(alpha, beta, theta2) + (1 - w[1] - w[2]) * twoPL(alpha, beta, theta1) * twoPL(alpha, beta, theta2)
 }
 
@@ -191,3 +192,77 @@ l <- outer(a1, a2, plot_neglogWAI, resp[i,], alpha, beta, theta1[i], theta2[i])
 persp(a1, a2, l, theta = 55, phi = 20, main = "Min Model", expand = .5, ylab = "a2")
 
 hist(l, breaks = 200)
+
+
+# More SEs -----------------------------------------------------------------------
+alpha = 1
+beta = 0
+theta1 = 1
+theta2 = 0
+x = 1
+w <- seq(0, 1, by = .01)
+info_w <- function(w, alpha, beta, theta1, theta2){
+  R <- lapply(w, function (x) WAI(c(x, 0), alpha, beta, theta1, theta2)) %>% unlist
+  P1 <- twoPL(alpha, beta, theta1)
+  P2 <- twoPL(alpha, beta, theta2)
+  1 / sqrt((P1*(1-P2))^2 / (1-R) / R)
+}
+
+plot(w, info_w(w, 1, 0, 0, 0))
+
+#  -----------------------------------------------------------------------
+
+alpha <- seq(0, 3, by = .01)
+beta = 0
+theta1 = 0
+theta2 = 0
+x = 1
+w = 1
+info_alpha <- function(alpha, w, beta, theta1, theta2){
+  R <-lapply(alpha, function (x) WAI(c(w, 0), x, beta, theta1, theta2)) %>% unlist
+  P1 <- twoPL(alpha, beta, theta1)
+  P2 <- twoPL(alpha, beta, theta2)
+  1/ sqrt(1/(P1*(1-P2))^2 / (1-R) / R)
+}
+
+plot(alpha, info_alpha(alpha, 1, .5, 0, 1))
+
+
+theta1 <- theta2 <- seq(-3, 3, by = .1)
+beta = 0
+alpha = 1
+x = 1
+w = 1
+
+info_theta <- function(theta1, theta2, w, alpha, beta){
+  R <- mapply(function(x, y) WAI(c(w, 0), alpha, beta, x, y), theta1, theta2) %>% unlist
+  P1 <- twoPL(alpha, beta, theta1)
+  P2 <- twoPL(alpha, beta, theta2)
+  1 / sqrt((P1*(1-P2))^2 / (1-R) / R)
+}
+
+l <- outer(theta1, theta2, info_theta, w, alpha, beta)
+l[l > 10] = NA
+persp(theta1, theta2, l, theta = -25, phi = 20, ticktype = "detailed", nticks = 5)
+
+hist(l, breaks = 200)
+
+
+par(bg = "white")
+x <- seq(-1.95, 1.95, length = 30)
+y <- seq(-1.95, 1.95, length = 35)
+z <- outer(x, y, function(a, b) a*b^2)
+nrz <- nrow(z)
+ncz <- ncol(z)
+# Create a function interpolating colors in the range of specified colors
+jet.colors <- colorRampPalette( c("blue", "green") )
+# Generate the desired number of colors from this palette
+nbcol <- 100
+color <- jet.colors(nbcol)
+# Compute the z-value at the facet centres
+zfacet <- z[-1, -1] + z[-1, -ncz] + z[-nrz, -1] + z[-nrz, -ncz]
+# Recode facet z-values into color indices
+facetcol <- cut(zfacet, nbcol)
+persp(x, y, z, col = color[facetcol], phi = 30, theta = -30)
+
+par(op)
